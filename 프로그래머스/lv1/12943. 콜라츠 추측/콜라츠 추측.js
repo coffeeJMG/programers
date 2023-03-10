b51ddaf96aaa0013d45a3e1192f1b416ebe1a68a
coffeeJMG <114897197+coffeeJMG@users.noreply.github.com>
2023-03-10 21:04:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -6,15 +6,15 @@ function solution(num) {
         
     if(num !==1){
         if(num%2 ===0){
-            num= num/2;
+            num= num/2;         //짝수 계산
         }else{
-            num=num*3 +1;
+            num=num*3 +1;       //홀수 계산
         }
     }else{
-        return answer =i;
+        return answer =i;       //num 이 1인경우 이므로 i가 0으로 바로 빠져나옴
     }
         
-      if(i===499){
+      if(i===499){              //500번째 인 경우 -1;
           answer =-1
       }
     }
@@ -22,7 +22,8 @@ function solution(num) {
     return answer
    
 }
-// 짝수인 경우와 홀수인 경우로 나누어 계산을 시작해 결과값을 저장
-// 결과값이 1일때까지 반복하고 1이면 1반환
+// num이 1이 아니라면
+// 짝수인 경우와 홀수인 경우로 나누어 
+// 계산을 시작해 결과값을 저장
 // 최대 반복 횟수 500을 넘으면 -1 반환
 // num 이 1인경우 0반환 
