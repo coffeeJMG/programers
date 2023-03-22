d49da8d759e8c7b1af3a0603c0270cf036471125
coffeeJMG <114897197+coffeeJMG@users.noreply.github.com>
2023-03-22 19:14:46 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -2,14 +2,16 @@ function solution(keyinput, board) {
     var answer = [];
     let x =0;
     let y =0;
+    let rangeX =Math.floor(board[0]/2);
+    
     for( i=0; i<keyinput.length; i++){
-        let item = keyinput[i];
-       if( item === "left"){
-           if(Math.abs(x) <Math.floor(board[0]/2) || x===Math.floor(board[0]/2)){
+        let item = keyinput[i];           
+       if( item === "left"){             
+           if(Math.abs(x) <rangeX || x===rangeX){
               x--
            }
         }else if( item === "right"){
-            if(Math.abs(x) < Math.floor(board[0]/2) || x*-1===Math.floor(board[0]/2)){
+            if(Math.abs(x) < rangeX || x*-1===rangeX){
                x++
             }
         }else if( item === "down"){
