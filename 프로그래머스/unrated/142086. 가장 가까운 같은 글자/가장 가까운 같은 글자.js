d09f41d072af00423e9234f76446e766b6facd8e
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2023-07-13 20:16:09 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 200.39 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,27 +1,27 @@
 function solution(s) {
     var answer = [];
     let count =0;
-    let distance=[];
-    for( i=0; i<s.length; i++){
+    let distance=[];                        // 같은 값이 있을경우 거리 값 저장배열
+    
+    
+    for( i=0; i<s.length; i++){             // 먼저 s[i] 번째를 0부터 순서대로 돈다
         let item=s[i];
-        for(j=0; j<i; j++){
+        for(j=0; j<i; j++){                 // 다음으로 s[j]값은 0부터 i보다 작을때까지 돈다.
           
-            if(item ===s[j]){
-                count+=1
-                distance.push(i-j)
+            if(item ===s[j]){               // 만약 같은 값이 있다면
+                count+=1                    // count를 늘리고
+                distance.push(i-j)          // 두 원소 거리를 차이배열에 추가한다.
                 
                }
           
          }
-       
-       
-        if(count===0){
+       if(count===0){                       // 같은 값이 없다면 -1을 정답에추가
             answer.push(-1)
-         }else{
-               answer.push(distance[distance.length-1])
-              distance=[];
+         }else{                                        // 같은 값이 있는 s[i] 번째라면 해당 순서에서
+              answer.push(distance[distance.length-1]) // 차이배열 중 가장 뒤쪽 즉 순서상 가장 가까운쪽 원소추가
+              distance=[];                             // 다음 s[i] 번째 차이를 비교하기 위해 차이배열 초기화;
          }
-        count=0;
+        count=0;                                       // 카운트또한 다음 순서비교를 위해 초기화
        
     }
     console.log(distance)
