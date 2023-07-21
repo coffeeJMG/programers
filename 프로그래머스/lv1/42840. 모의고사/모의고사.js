3f10d540798cbddd7f6ff7fbd590a30b94835884
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2023-07-21 16:58:33 +0900

    [level 1] Title: 모의고사, Time: 6.74 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,43 +1,27 @@
 function solution(answers) {
-    var answer = [];
-    let one = [1,2,3,4,5]
-    let two = [ 2, 1, 2, 3, 2, 4, 2, 5]
-    let three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    
-    let count =[0,0,0];
+    var answer = [0,0,0];
+    let result=[];
+    let first = [1,2,3,4,5]
+    let second = [2, 1, 2, 3, 2, 4, 2, 5]
+    let third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
     for(i=0; i<answers.length; i++){
-        if(answers[i]===one[i%5]){
-            count[0]++
+        if(answers[i] === first[i%5]){
+            answer[0]++
         }
-        if(answers[i]===two[i%8]){
-             count[1]++
+         if(answers[i] === second[i%8]){
+            answer[1]++
         }
-        if(answers[i]===three[i%10]){
-             count[2]++
-        }
-      }
-    let maxNum=0;
-    for(i=0; i<count.length; i++){
-        let item= count[i];
-        if(item>maxNum){
-            maxNum=item;
+         if(answers[i] === third[i%10]){
+            answer[2]++
         }
     }
-    
-    for(i=0; i<count.length; i++){
-        let item=count[i];
-        if(item===maxNum){
-            answer.push(i+1)
-        }
-    }
-    
-    console.log(count)
     console.log(answer)
-    return answer;
+    for(i=0; i<answer.length; i++){
+        if(answer[i] ===Math.max(...answer)){
+           result.push(i+1)
+        }
+    }
+   
+    return result;
 }
-
-//먼저 1,2,3번 수포자의 찍는 방식을 표현할 방법을 찾아야함
-// 배열로 찾아서 포문 돌리면서 같을때마다 count ++ 
-// count 높은사람 반환
-
