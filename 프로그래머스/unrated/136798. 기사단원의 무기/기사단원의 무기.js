ad130243a8a4d3552bc4d1e97a084eb4533a29a3
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2023-07-27 23:45:15 +0900

    [unrated] Title: 기사단원의 무기, Time: 3.37 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,43 +1,27 @@
 function solution(number, limit, power) {
     var answer = 0;
+    let divi = 0;
+    let array= [];
     
-   
-    let count=0;      // 공격력 변수
-    let attack=[];    // 기사단원의 공격력 변수(약수의 개수)
-    
-   
-    
-    for(i=1; i<=number; i++){
-       
-        for(j=1; j<=i/2; j++){         // 각 기사단원의 약수의 개수를 구함
-            if(i%j===0){
-                count++
+    for(cur=1; cur<=number; cur++){
+        
+        for(i=1; i<=cur/2; i++){
+            if(cur%i ===0){
+                divi++
             }
         }
-        attack.push(count+1)              // 약수의 개수를 attack에 추가하고 초기화
-        count=0;
+        array.push(divi+1)
+        divi=0;
     }
     
-    for(i=0; i<attack.length; i++){     
-         
-        if(attack[i]>limit){           // 공격력이 제한수치를 넘으면 
-           attack[i]=power;            // power 로 변경 
-        }else{
-            continue;
+    const final = array.filter((i)=> i<=limit)
+    
+    for(i=0; i<array.length; i++){
+        if(array[i]>limit){
+            array[i]=power
         }
-        
-         
+        answer+=array[i]
     }
-  
-    
-    
-    return attack.reduce((acc, cur) => acc + cur, 0);
+   
+    return answer;
 }
-
-
-
-
-
-// 기사 단원의 번호는 1~number까지
-// 약수의 개수는 1~limit 까지
-// limit을 안넘으면 공격력은 자신의 번호 약수의 개수, 넘으면 power 
