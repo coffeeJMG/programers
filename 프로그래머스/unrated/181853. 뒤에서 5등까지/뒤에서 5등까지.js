c204c417de7c22dfc4d0c11d014be8e0481520bd
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2023-08-24 23:59:11 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = [];
+    
+    num_list.sort((a,b)=>a-b)
+   
+    for(let i=0; i<5 ; i++){
+        answer.push(num_list[i])
+    }
+    return answer;
+}
